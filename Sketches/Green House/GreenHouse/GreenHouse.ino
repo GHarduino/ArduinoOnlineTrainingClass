bf352a6d5dd36da51d835d7508b81e92578f14be
AungWinnHtut <aungwinnhtut@gmail.com>
2017-03-01 00:06:38 +0630

    update greenhouse aircon data

--- Sketches/Green House/GreenHouse/GreenHouse.ino
+++ Sketches/Green House/GreenHouse/GreenHouse.ino
@@ -43,11 +43,16 @@ void setup()
 }
 
 void loop() {
+  int counter = 0;
+  int i = 0;
   int khz = 38; // 38kHz carrier frequency for the NEC protocol
   unsigned int irSignal1[] = {4450, 4450, 550 , 1700 , 550 , 1650, 550, 1700 , 500, 600, 550, 600 , 500, 550, 550, 600, 500, 600, 500, 1700, 550 , 1700, 500, 1700, 550, 600, 500, 600, 500, 600, 500, 600, 550, 550, 550, 1700 , 500, 1700, 550, 1650, 550, 600, 500, 600, 550 , 550, 550, 600, 500, 600, 500, 600, 550, 550 , 550, 600, 500 , 1700 , 500, 1700 , 550, 1700, 500, 1700, 550 , 1650, 550 };
   unsigned int irSignal2[] = {4500, 4450, 550 , 1700, 500, 1700, 550, 1650, 550, 600, 500, 600, 550, 600, 500, 600, 500, 600, 500, 1700 , 550, 1700, 500 , 1700, 550 , 550 , 550, 600, 500, 600, 500 , 600, 550, 550, 550, 1700, 500, 1700, 550, 600, 500, 1700, 500, 600, 550, 550, 550 , 550, 550, 600, 500 , 600 , 550 , 550, 550, 1650, 550, 600, 500 , 1700, 550, 1700, 500, 1700, 550, 1650, 550 };
-
+  unsigned int irPower[] = {700, 3550, 800, 3400, 800, 3400, 800, 3400, 850, 1350, 800 , 1350, 850, 3400, 800, 1350, 850 , 1400, 800, 1350, 800, 1350, 850, 1350 , 850, 3350 , 850, 3400, 800, 1350, 800, 3400, 950, 3400, 800, 3400, 800, 1350, 800, 1400, 800, 3400, 800, 3400, 850, 3400, 800, 3400, 850, 1350, 850, 1350, 800, 3400, 800, 3400, 850, 1300, 850, 1350, 850, 1350, 800 , 1350, 850, 1400, 800 , 3400, 800, 1350, 850, 3350 , 850, 1350, 850, 3350, 850, 1350, 800, 1350, 850, 3400, 850, 1350, 800, 3400 , 850 , 1350, 800, 3400, 800 , 1400, 800 , 3400, 800 , 3400, 800};
+  unsigned int irDown[] = {650, 3550, 800, 3400, 800, 3400 , 850, 3400 , 800, 1350 , 800 , 1400, 800, 3400 , 800 , 1350, 850, 1400, 800, 1350, 800 , 1400 , 800, 1350 , 850, 3350, 850, 3400, 800, 1350, 800, 3400, 950, 3400, 800 , 3400, 800 , 1350 , 800, 1400, 800, 3400, 800, 3400, 850, 1350, 800, 1350, 850, 1400, 800, 1350, 800, 3400, 850, 3700 , 800 , 1350, 850 , 1350, 850, 3400, 800, 3400, 850, 1350, 850, 3400, 800, 1350, 800, 3400, 800, 1350 ,850, 3400, 850, 1350, 800 ,1350, 850 ,3400, 850, 1350, 800, 3400, 800, 1350, 850, 3400 ,800 ,1350, 850, 3400 ,800, 3400, 800};
+  unsigned int irUp[] = {700, 3500 , 850, 3400 , 800 , 3400, 850, 3350, 850 , 1300, 900, 1300 , 850, 3350, 850, 1350, 900, 1300, 850, 1350 , 850, 1300, 850, 1300, 900, 3350 , 850, 3350, 850 , 1350, 800, 3400, 950, 3350, 850, 3350, 850, 1350, 850, 1300, 900, 3350, 850, 3350, 850, 3350, 850, 3400, 850, 1350, 850, 1300, 850 , 3400, 850, 3350, 850, 1300, 850, 1350, 850, 1300, 850, 1350, 900, 1300, 850, 3400, 800, 1350, 800, 3400, 850, 1300, 850, 3400, 800, 1350, 850, 1350, 900, 3350, 850, 1350 , 800 , 3400, 800, 1350, 850 , 3400, 800, 1350, 800 , 3400, 850 , 3400, 750};
   int chk = DHT.read22(DHT22_PIN);
+
   switch (chk)
   {
     case DHTLIB_OK:
@@ -120,10 +125,24 @@ void loop() {
   Serial.println(l + '@' + t + '@' + g + '@' + h );
 
   delay(300);
-  //irsend.sendRaw(irSignal1, sizeof(irSignal1) / sizeof(irSignal1[0]), khz); //Note the approach used to automatically calculate the size of the array.
 
-  // delay(5000); //In this example, the signal will be repeated every 5 seconds, approximately.
+  for (i = 0; i < 10; i++)
+  {
+    irsend.sendRaw(irPower, sizeof(irPower) / sizeof(irPower[0]), khz); //Note the approach used to automatically calculate the size of the array.
+    delay(1000); //In this example, the signal will be repeated every 5 seconds, approximately.
 
+    for (counter = 0; counter < 10; counter++)
+    {
+      irsend.sendRaw(irDown, sizeof(irDown) / sizeof(irDown[0]), khz); //Note the approach used to automatically calculate the size of the array.
+      delay(5000); //In this example, the signal will be repeated every 5 seconds, approximately.
+    }
+
+    for (counter = 0; counter < 10; counter++)
+    {
+      irsend.sendRaw(irUp, sizeof(irUp) / sizeof(irUp[0]), khz); //Note the approach used to automatically calculate the size of the array.
+      delay(5000); //In this example, the signal will be repeated every 5 seconds, approximately.
+    }
+  }
   //irsend.sendRaw(irSignal2, sizeof(irSignal2) / sizeof(irSignal2[0]), khz); //Note the approach used to automatically calculate the size of the array.
   // delay(5000); //In this example, the signal will be repeated every 5 seconds, approximately.
 }
